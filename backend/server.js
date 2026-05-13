17d108f13af9cfedd4178b35c6b20ee2cc0454fd
RAZAFITSALAMA-sandra <sandrarazafitsalama@gmail.com>
2026-05-13 13:16:01 +0300

    feat: backend code for the project

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from TaskFlow API");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+app.get("/health", (req, res) => {
+  res.json({ status: "OK" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Serveur sur le port ${PORT}`));
