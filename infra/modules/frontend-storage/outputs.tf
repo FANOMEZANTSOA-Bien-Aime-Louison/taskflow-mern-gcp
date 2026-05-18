3eeb6f01f41206d3fb3fe631fdf8786e769911c3
FANOMEZANTSOA Bien Aimé Louison <140715901+FANOMEZANTSOA-Bien-Aime-Louison@users.noreply.github.com>
2026-05-18 08:56:46 +0300

    Add frontend IP and CDN URL outputs

--- infra/modules/frontend-storage/outputs.tf
+++ infra/modules/frontend-storage/outputs.tf
@@ -2,6 +2,11 @@ output "frontend_bucket_name" {
   value = google_storage_bucket.frontend_bucket.name
 }
 
-output "frontend_bucket_url" {
-  value = "https://storage.googleapis.com/${google_storage_bucket.frontend_bucket.name}"
+output "frontend_ip" {
+  value       = google_compute_global_address.frontend_ip.address
+  description = "IP à pointer dans Cloudflare (enregistrement A)"
+}
+
+output "frontend_cdn_url" {
+  value = "https://${var.domain}"
 }
