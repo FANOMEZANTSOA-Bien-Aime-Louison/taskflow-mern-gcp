1e5623af0cb1a6492b502c0dd80caa87a65eb14a
FANOMEZANTSOA Bien Aimé Louison <140715901+FANOMEZANTSOA-Bien-Aime-Louison@users.noreply.github.com>
2026-05-18 08:59:17 +0300

    Add frontend_ip output for Load Balancer IP

--- infra/environnements/dev/outputs.tf
+++ infra/environnements/dev/outputs.tf
@@ -5,3 +5,8 @@ output "vpc_name" {
 output "subnet_name" {
   value = module.network.subnet_name
 }
+
+output "frontend_ip" {
+  value       = module.frontend_storage.frontend_ip
+  description = "IP du Load Balancer frontend à mettre dans Cloudflare"
+}
